ba3ee4a8d9012b80de150afc8a4a0d2a7465323f
u10k <2421113739@qq.com>
2017-09-13 23:17:49 +0800

    Create string.go

---
+++ string.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str string
+	str = "Go语言"
+	fmt.Println(len(str))
+	fmt.Println(str)
+	fmt.Printf("%c\n",str[0])
+	fmt.Printf("%c\n",str[1])
+	fmt.Printf("%c\n",str[2])
+	fmt.Printf("%c\n",str[3])
+	fmt.Printf("%c\n",str[4])
+	fmt.Printf("%c\n",str[5])
+	fmt.Printf("%c\n",str[6])
+	fmt.Printf("%c\n",str[7])
+}
