a68f77670b5501b78d36436ccec4a0a82612a558
u10k <2421113739@qq.com>
2017-09-15 02:45:44 +0800

    Create const2.go

---
+++ const2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main()  {
+	/*const a int = 1;
+	fmt.Print(a)*/
+	/*const (
+		a = 1
+		b = "hello"
+		c = true
+	)*/
+	const (
+		a = 1
+		b
+		c
+		d = "hello"
+		e
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+}
